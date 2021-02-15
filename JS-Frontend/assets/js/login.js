066fc8dcf007c18295fce072c6d0dbb5e928bfe8
abdelfatah <abdelfatahhezema@gmail.com>
2021-02-15 16:52:06 +0200

    Frontend landing page

---
+++ JS-Frontend/assets/js/login.js
@@ -0,0 +1,17 @@
+const signInBtn = document.querySelector("#sign-in-btn");
+const signUpBtn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+signUpBtn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+signInBtn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     container.animate({
+//         opacity: [0, 1]
+//     }, 400);
+//   });
