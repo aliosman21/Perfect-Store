734cb5e5bc8a1b6a997229b8df054b459060ef22
abdelfatah <abdelfatahhezema@gmail.com>
2021-02-15 16:47:02 +0200

    Working on landing page

--- JS-Backend/server.js
+++ JS-Backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const registerRoute = require("./Components/Users/RegisterRoute");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const path = require("path");
 //const MongoClient = require("mongodb").MongoClient;
 
 dotenv.config();
@@ -13,9 +14,12 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../JS-Frontend/assets")));
 app.use("/User/register", registerRoute);
 
 //app.use("<route custom name>", const <route name>)
 //app.use("/", (req, res) => {});
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../JS-Frontend/index.html"));
+});
 app.listen(port, () => console.log("Server is up on port " + port));
