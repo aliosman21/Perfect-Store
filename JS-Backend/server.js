e9dc6192bef16c81279a94e011fde9aea27c1556
Omar Yehia <oyehia94@gmail.com>
2021-02-17 17:25:05 +0200

    Added pages container to products pages + modifications to the cards

--- JS-Backend/server.js
+++ JS-Backend/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-//app.use(express.static(path.join(__dirname, "../JS-Frontend/assets")));
+app.use(express.static(path.join(__dirname, "../JS-Frontend/assets")));
 app.use("/User/register", registerRoute);
 app.use("/User/login", loginRoute);
 app.use("/products/addNewProduct", productRoute);
