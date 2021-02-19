246a6c6e54467b6d7e8a9ce77935151f0505e424
Omar Yehia <oyehia94@gmail.com>
2021-02-19 18:48:42 +0200

    Fetch requests for login and signup

--- JS-Frontend/assets/js/login.js
+++ JS-Frontend/assets/js/login.js
@@ -1,6 +1,8 @@
 const signInBtn = document.querySelector("#sign-in-btn");
 const signUpBtn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
+const signIn = document.querySelector("#sign-in");
+const signUp = document.querySelector("#sign-up");
 
 signUpBtn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
@@ -10,8 +12,55 @@ signInBtn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     container.animate({
-//         opacity: [0, 1]
-//     }, 400);
-//   });
+signIn.addEventListener("click", (e) => {
+  e.preventDefault();
+  login();
+})
+
+signUp.addEventListener("click", (e) => {
+  e.preventDefault();
+  signup();
+})
+
+async function login() {
+  let response = await fetch("http://localhost:5000/User/login/authenticateLogin", {
+    method: "POST",
+    
+    body: JSON.stringify({
+      username: document.querySelector("#sign-in-username").value,
+      password: document.querySelector("#sign-in-password").value
+    })
+  });
+
+  let jsonRes = await response.json();
+
+  console.log(jsonRes);
+  if (jsonRes["success"] == false) {
+    let errorEl = document.querySelector("#error-message");
+    errorEl.innerText = jsonRes["message"];
+  }
+}
+
+async function signup() {
+  let response = await fetch ("http://localhost:5000/User/register/addUser", {
+    method: "POST",
+    body: JSON.stringify({
+      username: document.querySelector("#sign-up-username").value,
+      email: document.querySelector("#sign-up-email").value,
+      address: document.querySelector("#address").value,
+      password: document.querySelector("#sign-up-password").value
+    })
+  });
+
+  let jsonRes = await response.json();
+
+  if(jsonRes["statusType"] === "Confirmation") {
+    console.log("Successssssssssssssssssssss");
+  }
+
+
+  if (jsonRes["success"] == false) {
+    let errorEl = document.querySelector("#error-message");
+    errorEl.innerText = jsonRes["message"];
+  }
+}
