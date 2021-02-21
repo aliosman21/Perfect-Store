02b70b7f34923442287edd7eacfce290776ae1b3
aliosman21 <amrali975@gmail.com>
2021-02-21 17:44:49 +0200

    Edit products done

---
+++ JS-Backend/Components/Products/editProduct.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const productsSchema = require("./ProductModel");
+const connectionToDB = require("../DBConnector/ConnectionHandler");
+
+router.patch("/", async (req, res) => {
+   connectionToDB.establishConnection();
+   const productData = await productsSchema.findOne({ _id: req.body.id });
+   let modifications = {};
+
+   if (productData) {
+      console.log("Found him");
+      if (req.body.name) {
+         modifications.name = req.body.name;
+      }
+      if (req.body.quantity) {
+         modifications.quantity = req.body.quantity;
+      }
+      if (req.body.description) {
+         //hashed password
+         modifications.description = req.body.description;
+      }
+      if (req.body.price) {
+         modifications.price = req.body.price;
+      }
+      try {
+         if (req.file.path) {
+            modifications.image = "http://localhost:5000/" + req.file.path;
+         }
+      } catch {
+         console.log("no image");
+      }
+
+      await productsSchema.findByIdAndUpdate(productData._id, { $set: modifications });
+
+      res.send({ success: true, message: "Product Updated" });
+   } else {
+      console.log("NOT FOUND");
+      res.send({ success: false, message: "Something went wrong" });
+   }
+
+   connectionToDB.closeConnection();
+});
+
+module.exports = router;
